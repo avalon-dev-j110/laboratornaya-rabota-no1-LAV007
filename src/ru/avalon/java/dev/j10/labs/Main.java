bf1faac2f4ea347cea726b2c2a2dbfc004d71375
Alex <61654925+LAV007@users.noreply.github.com>
2020-04-13 10:37:27 +0300

    Update Main.java

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -5,22 +5,22 @@ import ru.avalon.java.dev.j10.labs.commons.*;
 
 public class Main {
     public static void main(String[] args) {
-        Person smith = new Person(new Passport("John","Edvard","Smith"),
+        Person smith = new Person(1, new Passport("John","Smith"),
                        new Address("U.S.A", "15848",  "California",  "Los Angeles",  "Gold\'s Gym",  "34",  "7"));              
-        String fullNameSmith = smith.getFullName(false);
+        String fullNameSmith = smith.getFullName();
         String addressSmith = smith.getAddress();
         
-        Person ivanov = new Person(new Passport("Ivan","Ivanovich","Ivanov"),
+        Person ivanov = new Person(2, new Passport("Ivan","Ivanovich","Ivanov"),
                         new Address("Russia", "188800", "Leningradskaya obl.", "Vyborg", "Turgeneva", "9", "25"));              
-        String fullNameIvanov = ivanov.getFullName(false);
+        String fullNameIvanov = ivanov.getFullName();
         String addressIvanov = ivanov.getAddress();
         
-        Person conor = new Person(new Passport("Conor","Anthony ","McGregor"),
+        Person conor = new Person(3, new Passport("Conor","Anthony ","McGregor"),
                        new Address("Ireland", "45957", "Laighean", "Dublin", "Wall street", "24", "57"));              
-        String fullNameConor = conor.getFullName(true);
+        String fullNameConor = conor.getFullName();
         String addressConor = conor.getAddress();       
         
-        Person ferguson = new Person(new Passport("Anthony", "Ferguson"),
+        Person ferguson = new Person(1, new Passport("Anthony", "Ferguson"),
                           new Address("U.S.A", "75521", "California", "Oxnard", "Ventura street", "75", "35"));              
         String fullNameFerguson = ferguson.getFullName();
         String addressFerguson = ferguson.getAddress();
